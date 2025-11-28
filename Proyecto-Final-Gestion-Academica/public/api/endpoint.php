bc467addf6aa4fca32d0da6244a6f3f1c9372c9d
vicvaporoub <0265786@up.edu.mx>
2025-11-28 02:30:27 -0600

    Fix SQL query to reorder selected fields

--- Proyecto-Final-Gestion-Academica/public/api/endpoint.php
+++ Proyecto-Final-Gestion-Academica/public/api/endpoint.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: http://localhost:4200");  
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 
 // Consulta simple de todos los docentes
 $sql = "
-    SELECT id_docente, nombre, academia, grado, idioma, sni, status, correo
+    SELECT id_docente, nombre, correo, academia, grado, idioma, sni, status
     FROM docentes
 ";
 
