f018cd9980fa5ea16dae6148778a1c50e307466c
vicvaporoub <0265786@up.edu.mx>
2025-11-28 02:14:04 -0600

    Enhance docente management with edit, delete, and export
    Added functionality for editing and deleting docentes, including modal handling and CSV export.

--- Proyecto-Final-Gestion-Academica/src/app/pages/docentes/docentes.ts
+++ Proyecto-Final-Gestion-Academica/src/app/pages/docentes/docentes.ts
@@ -34,6 +34,7 @@ export class Docentes {
   terminoBusqueda: string = '';  // texto del input de búsqueda
 
   nuevoDocente = {
+    id: '',
     nombre: '',
     correo: '',
     academia: '',
@@ -60,14 +61,15 @@ export class Docentes {
     { id: 5, nombre: 'Dra. Rosa Torres', area: 'Ingeniería', grado: 'Doctorado', sni: 'Nivel III', status: 'Inactivo' },
   ];
    */
- 
+
   async guardarDocente() {
     await this.sql.agregarDocente(this.nuevoDocente);
 
-    const btnClose = document.querySelector('[data-dismiss="modal"]') as HTMLElement;
+const btnClose = document.querySelector('#modalAgregarDocente [data-dismiss="modal"]') as HTMLElement;
     if (btnClose) btnClose.click();
 
     this.nuevoDocente = {
+      id: '',
       nombre: '',
       correo: '',
       academia: '',
@@ -82,7 +84,7 @@ export class Docentes {
 
   }
   async cargarDocentes() {
-    this.docentes = await this.sql.conectarAPI();  // tu función que hace el $.ajax GET
+    this.docentes = await this.sql.conectarAPI(); //await detiene la ejecución de ngOnInit hasta que la promesa se resuelve y asigna el resultado a this.docentes.
     this.docentesFiltrados = [...this.docentes]; //copia de docentes para que al inicio la tabla muestre todo
   }
 
@@ -90,7 +92,7 @@ export class Docentes {
     // Pasamos lo que escribió el usuario a minúsculas y sin espacios extra
     const term = this.terminoBusqueda.toLowerCase().trim();
 
-    // Si no hay nada escrito → mostrar todos
+    // Si no hay nada escrito entonces mostrar todos
     if (!term) {
       this.docentesFiltrados = [...this.docentes];
       return;
@@ -103,6 +105,7 @@ export class Docentes {
       const nombre = (d.nombre || '').toLowerCase();
       const academia = (d.academia || '').toLowerCase();
       const grado = (d.grado || '').toLowerCase();
+      const idioma = (d.idioma || '').toLowerCase();
       const sni = (d.sni || '').toLowerCase();
       const status = (d.status || '').toLowerCase();
 
@@ -112,10 +115,96 @@ export class Docentes {
         nombre.includes(term) ||
         academia.includes(term) ||
         grado.includes(term) ||
+        idioma.includes(term) ||
+
         (sni == term) ||
         (status == term)
 
       );
     });
   }
+  docenteEditar = {
+    id: '',
+    nombre: '',
+    correo: '',
+    academia: '',
+    grado: '',
+    idioma: '',
+    sni: '',
+    status: ''
+  };
+  docenteEliminar = {
+    id: '',
+    nombre: '',
+    correo: '',
+    academia: '',
+    grado: '',
+    idioma: '',
+    sni: '',
+    status: ''
+  };
+
+  abrirModalEditar(docente: any) {
+    this.docenteEditar = { ...docente };
+  };
+
+  async actualizarDocente() {
+    await this.sql.editarDocente(this.docenteEditar);
+
+const btnClose = document.querySelector('#modalEditarDocente [data-dismiss="modal"]') as HTMLElement;    
+if (btnClose) btnClose.click();
+
+    this.docenteEditar= {
+      id: '',
+      nombre: '',
+      correo: '',
+      academia: '',
+      grado: '',
+      idioma: '',
+      sni: '',
+      status: ''
+    }
+    // refrescar la tabla
+    this.cargarDocentes();
+
+
+  }
+  async eliminarRegistro(index:number, docente:any){
+    if (!confirm("¿Estás seguro de eliminar a " + docente.nombre + "?")) {
+    return; // Si dice cancelar, no hacemos nada
+  }
+  await this.sql.eliminarDocenteBD(docente.id_docente);
+  this.docentes.splice(index, 1);
+  
+  this.cargarDocentes();
+
+  }
+exportarCSV() {
+
+  if (!this.docentes || this.docentes.length === 0) {
+    alert("No hay datos para exportar.");
+    return;
+  }
+
+  let csvContent = "data:text/csv;charset=utf-8,";
+
+  // Para evitar errores con acentos en Excel
+  csvContent += "\uFEFF";
+
+  csvContent += "LISTADO DE DOCENTES\n";
+  csvContent += "ID,Nombre,Academia,Grado,Idioma,SNI,Status\n";
+
+  this.docentes.forEach((d: any, index: number) => {
+    csvContent += `${index + 1},"${d.nombre}","${d.academia}","${d.grado}","${d.idioma}","${d.sni}","${d.status}"\n`;
+  });
+
+  const encodedUri = encodeURI(csvContent);
+  const link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "lista_docentes.csv");
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 }
